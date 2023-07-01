3dd48425e74c3827ae84f73b6428fe9833919066
Volodymyr Migda <volodymyr.migda@brightechstudio.com>
2023-07-01 10:48:27 +0300

    HW №7

---
+++ js/index.js
@@ -0,0 +1,60 @@
+// Функція, яка запитує у користувача значення, доки воно не буде коректним або доки користувач не відмовиться надавати відповідь:
+const promptUntilValid = (promptMessage) => {
+  while (true) {
+    const input = prompt(promptMessage);
+    if (!input) {
+      alert("Жаль. Сподіваюсь ще побачитись.");
+      return null;
+    }
+    const number = Number(input);
+    if (!isNaN(number)) {
+      return number;
+    }
+  }
+}
+
+// Функція знаходження коренів квадратного рівняння:
+const solveQuadraticEquation = (a, b, c) => {
+  // Дискриминант обчислюэться за формулою b^2-4ac
+  const discriminant = b * b - 4 * a * c;
+  if (discriminant < 0) {
+    alert("D < 0. Рівняння не має рішень");
+  } else if (discriminant === 0) {
+    /*
+      x = -(b/(2*a)
+    */
+    const x = -b / (2 * a);
+    alert("D = 0. Єдиний корінь рівняння: " + x);
+  } else {
+    /*
+      x1 = (-b + √D)/2*a
+      x2 = (-b - √D)/2*a
+    */
+    const x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    const x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    alert("D > 0. Корені рівняння: " + x1 + ", " + x2);
+  }
+}
+
+const quadraticEquationSolver = () => {
+  alert("Будемо знаходити рішення квадратного рівняння виду: ax^2 + bx + c = 0");
+
+  const a = promptUntilValid("Введіть a");
+  if (a === null) {
+    return;
+  }
+
+  const b = promptUntilValid("Введіть b");
+  if (b === null) {
+    return;
+  }
+
+  const c = promptUntilValid("Введіть c");
+  if (c === null) {
+    return;
+  }
+
+  solveQuadraticEquation(a, b, c);
+}
+
+quadraticEquationSolver();
