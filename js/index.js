fc687910a5868f7a4254a91dcbbff2d36768e34f
Volodymyr Migda <volodymyr.migda@brightechstudio.com>
2023-07-12 16:22:47 +0300

    hw 8

--- js/index.js
+++ js/index.js
@@ -1,58 +1,68 @@
-// Функція, яка запитує у користувача значення, доки воно не буде коректним або доки користувач не відмовиться надавати відповідь:
-const promptUntilValid = (promptMessage) => {
-  while (true) {
-    const input = prompt(promptMessage);
-    if (input === null) {
-      alert("Жаль. Сподіваюсь ще побачитись.");
+function program() {
+  function getNumericValue(queryString, titleString = '') {
+
+    let isFirstRequest = true,
+      value;
+    do {
+      // якщо titleString не пустий він буде відображатися
+      value = prompt(((isFirstRequest && titleString) ? titleString + '\n' : '') + queryString);
+      // перемикаємо флаг щоб повторно не показувати titleString
+      isFirstRequest = false;
+    } while ((!value || isNaN(value)) && value !== null);
+
+    return value === null ? value : +value;
+  }
+
+  function squareEquation(a, b, c, invalidCallback) {
+    if (a === 0) {
+      // перевырта що в invalidCallback прийшла функцыя та виклик її
+      if (typeof invalidCallback === 'function') invalidCallback();
       return null;
     }
-    if (!isNaN(Number(input)) && input !== '' && Number(input) !== 0)
-      return input;
+    // Дискрімінант
+    const d = b * b - 4 * a * c;
+    if (d < 0) {
+      return {
+        d: d,
+        x1: null,
+        x2: null
+      };
+    } else {
+      const sqrtD = Math.sqrt(d),
+        x1 = (-b + sqrtD) / (2 * a),
+        x2 = (d === 0) ? x1 : (b + sqrtD) / (2 * a);
+
+      return {
+        d: d,
+        x1: x1,
+        x2: x2
+      };
+    }
   }
+  // Користувач нажа Скасувати
+  function goFromApp() {
+    alert('Жаль. Сподіваюсь ще побачитись');
+  }
+
+  const a = getNumericValue('Введить a.', 'Будемо знаходить рішення квадратного рівняння виду: ax^2 + bx + c = 0');
+  if (a === null) return goFromApp();
+
+  const b = getNumericValue(`a=${a}\nВведить b`, '');
+  if (b === null) return goFromApp();
+
+  const c = getNumericValue(`a=${a},b=${b}\nВведить c`, '');
+  if (c === null) return goFromApp();
+
+  const result = squareEquation(a, b, c, () => alert('a = 0. Рівняння не має рішень'));
+  if (result === null) return null;
+
+  if (result.d < 0) {
+    alert('D < 0. Рівняння не має рішень');
+    return;
+  }
+
+  alert(result.d === 0 ? `D = 0. Єдиний корень рівняння: ${result.x1}` : `D > 0. Корені рівняння: ${result.x1}, ${result.x2}`)
 }
 
-// Функція знаходження коренів квадратного рівняння:
-const solveQuadraticEquation = (a, b, c) => {
-  // Дискриминант обчислюэться за формулою b^2-4ac
-  const discriminant = b * b - 4 * a * c;
-  if (discriminant < 0)
-    alert("D < 0. Рівняння не має рішень");
-  else if (discriminant === 0) {
-    /*
-      x = -(b/(2*a)
-    */
-    const x = -b / (2 * a);
-    alert("D = 0. Єдиний корінь рівняння: " + x);
-  } else {
-    /*
-      x1 = (-b + √D)/2*a
-      x2 = (-b - √D)/2*a
-    */
-    const x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-    const x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-    alert("D > 0. Корені рівняння: " + x1 + ", " + x2);
-  }
-}
 
-const quadraticEquationSolver = () => {
-  let a = prompt("Будемо знаходити рішення квадратного рівняння виду: ax^2 + bx + c = 0\n Введіть a");
-  if (a === null) {
-    alert("Жаль. Сподіваюсь ще побачитись.");
-    return
-  }
-  a = (!isNaN(Number(a)) && a !== '' && Number(a) !== 0  ) ? a : promptUntilValid("Введіть a");
-  if (a === null)
-    return;
-
-  const b = promptUntilValid(`a = ${a}\nВведіть b`);
-  if (b === null)
-    return;
-
-  const c = promptUntilValid(`a = ${a}, b = ${b}\nВведіть c`);
-  if (c === null)
-    return;
-
-  solveQuadraticEquation(a, b, c);
-}
-
-quadraticEquationSolver();
+program();
