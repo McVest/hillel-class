f1712a3be76066ef4ca7b8e6b4f4dee3e63e5d84
msgda <pixman28@gmail.com>
2023-08-06 19:43:43 +0300

    hw-19

---
+++ js/human.js
@@ -0,0 +1,10 @@
+export class Human {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  getHuman() {
+    console.log(`Name:${this.name}, Age: ${this.age}`);
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,36 +1,5 @@
-class Human {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  
-  getHuman() {
-    console.log(`Name:${this.name}, Age: ${this.age}`);
-  }
-}
-
-class Car {
-  owner;
-  
-  constructor(brand, model, licensePlate, yearOfManufacture) {
-    this.brand = brand;
-    this.model = model;
-    this.yearOfManufacture = yearOfManufacture;
-    this.licensePlate = licensePlate;
-  }
-  
-  setOwner(owner) {
-    owner.age >= 18 ? this.owner = owner : console.log('The owner is unknown. (since the age of eighteen)');
-  }
-  
-  getInfo() {
-    console.log(`Car: ${this.brand} ${this.model}, Year: ${this.yearOfManufacture}, License Plate: ${this.licensePlate}`);
-    console.log("Owner:")
-    this.owner ?
-      this.owner.getHuman() :
-      console.log("The owner is unknown.");
-  }
-}
+import { Human } from './human.js';
+import { Car } from './car.js';
 
 const human1 = new Human("John Doe", 25);
 human1.getHuman();
