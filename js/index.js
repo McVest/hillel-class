86faede9ac919384da55222207a7f504782e7682
Volodymyr <msgda@ukr.net>
2023-09-09 13:08:14 +0300

    fix hw-30

--- js/index.js
+++ js/index.js
@@ -39,20 +39,60 @@ function showProductDetails(productItems) {
   productInfo.innerHTML = '<p>Назва: ' + productItems.name + '</p>' +
     '<p>Категорія: ' + productItems.category + '</p>' +
     '<p>Ціна: ' + productItems.price + ' грн</p>' +
-    '<button onclick="buyProduct(\'' + productItems.name + '\')">Купити</button>';
+    '<button id="buy-button">Купити</button>';
   document.getElementById('product-details').appendChild(productInfo);
+  const buyButton = document.getElementById('buy-button');
+  buyButton.addEventListener('click', function () {
+    showOrderForm(productItems);
+  });
 }
 
-function getProductByName(productItems) {
-  return {
-    name: productItems.name,
-    category: productItems.category,
-    price: productItems.price
-  };
+function showOrderForm(product) {
+  console.log(product);
+  const orderForm = document.getElementById('order-form');
+  orderForm.style.display = 'block';
+  
+  const form = document.getElementById('order-details-form');
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const customerName = document.getElementById('customer-name').value;
+    const city = document.getElementById('city').value;
+    const novaPoshta = document.getElementById('nova-poshta').value;
+    const paymentMethod = document.querySelector('input[name="payment"]:checked');
+    const quantity = document.getElementById('quantity').value;
+    const comment = document.getElementById('comment').value;
+    const orderInfo = {
+      productName: product.name,
+      category: product.category,
+      price: product.price,
+      customerName: customerName,
+      city: city,
+      novaPoshta: novaPoshta,
+      paymentMethod: paymentMethod.value,
+      quantity: quantity,
+      comment: comment,
+    };
+    console.log(orderInfo);
+    displayOrderInfo(orderInfo);
+    orderForm.style.display = 'none';
+    document.getElementById('product-list').innerHTML = '';
+    document.getElementById('product-details').innerHTML = '';
+  })
 }
 
-function buyProduct(productName) {
-  alert('Товар ' + productName + ' куплений!');
-  document.getElementById('product-list').innerHTML = '';
-  document.getElementById('product-details').innerHTML = '';
+function displayOrderInfo(orderInfo) {
+  const orderDetails = document.createElement('div');
+  const store = document.querySelector('.store');
+  
+  orderDetails.innerHTML = '<h3>Інформація про замовлення</h3>' +
+    '<p>Назва товару: ' + orderInfo.productName + '</p>' +
+    '<p>Категорія: ' + orderInfo.category + '</p>' +
+    '<p>Ціна: ' + orderInfo.price + ' грн</p>' +
+    '<p>ПІБ покупця: ' + orderInfo.customerName + '</p>' +
+    '<p>Місто: ' + orderInfo.city + '</p>' +
+    '<p>Склад Нової пошти: ' + orderInfo.novaPoshta + '</p>' +
+    '<p>Спосіб оплати: ' + (orderInfo.paymentMethod === 'cash-on-delivery' ? 'Післяплата' : 'Оплата банківською карткою') + '</p>' +
+    '<p>Кількість: ' + orderInfo.quantity + '</p>' +
+    '<p>Коментар до замовлення: ' + orderInfo.comment + '</p>';
+  store.appendChild(orderDetails);
 }
